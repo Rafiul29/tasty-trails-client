85f6f922ed1b7539e3005d0795b5973ad8ee4ce1
Rafiul29 <rafiulislam4472@gmail.com>
2024-08-19 11:24:03 +0600

    redux setup done

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_APP_API_URL,  // Corrected here
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+      const token = getState()?.auth?.token;
+      if (token) {
+        headers.set("Authorization", `Token ${token}`);
+      }
+      return headers;
+    }
+  }),
+  tagTypes: [],
+  endpoints: (builder) => ({}),
+});
