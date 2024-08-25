5daad9ad4c7688a99c6efb2ac058f51c4f6a99e2
Rafiul29 <rafiulislam4472@gmail.com>
2024-08-26 01:12:48 +0600

    fix invalid token

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,19 +1,43 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-console.log(import.meta.env.VITE_APP_API_URL,'api')
+ import { userLoggedOut } from "../auth/authSlice.js";
+
+const baseQuery=fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_APP_API_URL,  // Corrected here
+  prepareHeaders: async (headers, { getState, endpoint }) => {
+    const token = getState()?.auth?.accessToken
+
+    if (token) {
+      headers.set("Authorization", `Token ${token}`);
+    }
+    return headers;
+  }
+ 
+})
+
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_APP_API_URL,  // Corrected here
-    prepareHeaders: async (headers, { getState, endpoint }) => {
-      const token = getState()?.auth?.accessToken
-      console.log({token})
-      if (token) {
-        headers.set("Authorization", `Token ${token}`);
-      }
-      return headers;
+  baseQuery: async(args,api,extraOptions)=>{
+    let result=await baseQuery(args,api,extraOptions)
+    console.log(result)
+    if(result?.error?.status===403){
+        api.dispatch(userLoggedOut())
+        localStorage.removeItem('auth')
     }
+    return result
+},
+  
+  // fetchBaseQuery({
+  //   baseUrl: import.meta.env.VITE_APP_API_URL,  // Corrected here
+  //   prepareHeaders: async (headers, { getState, endpoint }) => {
+  //     const token = getState()?.auth?.accessToken
+  //     console.log({token})
+  //     if (token) {
+  //       headers.set("Authorization", `Token ${token}`);
+  //     }
+  //     return headers;
+  //   }
    
-  }),
+  // }),
   tagTypes: [],
   endpoints: (builder) => ({}),
 });
