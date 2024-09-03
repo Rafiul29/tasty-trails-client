bb287b5b04e732434d767592ffc3ed38cd532500
Rafiul29 <rafiulislam4472@gmail.com>
2024-09-03 12:52:50 +0600

    login page update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,91 +27,89 @@ const Login = () => {
     }
     if (data?.token && data?.user) {
       navigate("/");
-      localStorage.removeItem('userEmail')
     }
   }, [data, responseError, navigate]);
 
   return (
     <main className="pt-24">
       <section className=" h-[calc(100vh-16rem)] flex items-center">
-
-      <div className="wrapper lg:w-3/5  ">
-        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-3 gap-0  items-center">
-          {/* col-1  login form*/}
-          <div>
-            <h2 className="text-center text-2xl  md:text-3xl font-extrabold text-gray-900 mb-3">
-              Sign in to your account
-            </h2>
-            <div className="max-w-md mx-auto">
-              {error !== "" && <Error message={error} />}
-            </div>
-            <form className="max-w-md mx-auto" onSubmit={handleLogin}>
-              <div className="relative z-0 w-full mb-3 group">
-                <label
-                  htmlFor="username"
-                  className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  User Name
-                </label>
-                <input
-                  type="text"
-                  id="username"
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 dark:shadow-sm-light"
-                  placeholder="Enter your user name"
-                  required
-                  onChange={(e) => setUserName(e.target.value)}
-                />
+        <div className="wrapper lg:w-3/5  ">
+          <div className="grid grid-cols-1 md:grid-cols-2 md:gap-3 gap-0  items-center">
+            {/* col-1  login form*/}
+            <div>
+              <h2 className="text-center text-2xl  md:text-3xl font-extrabold text-gray-900 mb-3">
+                Sign in to your account
+              </h2>
+              <div className="max-w-md mx-auto">
+                {error !== "" && <Error message={error} />}
               </div>
-
-              <div className="relative z-0 w-full mb-3 group">
-                <label
-                  htmlFor="password"
-                  className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 dark:shadow-sm-light"
-                  placeholder="Enter your password"
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <button
-                  disabled={isLoading}
-                  type="submit"
-                  className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
-                >
-                  {!isLoading ? "Submit" : <ButtonLoading />}
-                </button>
-                <p className="">
-                  <span >Don't have an account? </span>
-                  <Link
-                    to="/register"
-                    className="font-medium text-orange-500 ml-2 underline"
+              <form className="max-w-md mx-auto" onSubmit={handleLogin}>
+                <div className="relative z-0 w-full mb-3 group">
+                  <label
+                    htmlFor="username"
+                    className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Register Now
-                  </Link>
-                </p>
-              </div>
-            </form>
-          </div>
+                    User Name
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 dark:shadow-sm-light"
+                    placeholder="Enter your user name"
+                    required
+                    onChange={(e) => setUserName(e.target.value)}
+                  />
+                </div>
 
-          {/* col-2 */}
-          <div className="overflow-hidden md:block hidden">
-            <img
-              className="h-full w-full object-cover"
-              src={LoginImage}
-              alt="Login image"
-            />
+                <div className="relative z-0 w-full mb-3 group">
+                  <label
+                    htmlFor="password"
+                    className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 dark:shadow-sm-light"
+                    placeholder="Enter your password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-1">
+                  <button
+                    disabled={isLoading}
+                    type="submit"
+                    className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+                  >
+                    {!isLoading ? "Submit" : <ButtonLoading />}
+                  </button>
+                  <p className="">
+                    <span>Don't have an account? </span>
+                    <Link
+                      to="/register"
+                      className="font-medium text-orange-500 ml-2 underline"
+                    >
+                      Register Now
+                    </Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+
+            {/* col-2 */}
+            <div className="overflow-hidden md:block hidden">
+              <img
+                className="h-full w-full object-cover"
+                src={LoginImage}
+                alt="Login image"
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </main>
   );
 };
